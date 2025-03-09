749b4822803f2ce3c18011f3a4c3debb1bb733e7
YasuGoto <yasuko.goto@nijibox.co.jp>
2025-03-10 00:42:15 +0900

    fix: リクエスト許可

--- controllers/urls.js
+++ controllers/urls.js
@@ -13,7 +13,7 @@ const updateShort = async (req, res) => {
     const newUrl = new Url({ originalUrl, shortId });
 
     await newUrl.save();
-    const baseUrl = import.meta.env.BASE_URL || "http://localhost:3000";
+    const baseUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
     res.json({ shortUrl: `${baseUrl}/${shortId}` });
   } catch (err) {
     console.error("エラー発生:", err);
