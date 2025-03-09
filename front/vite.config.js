793d7133d937fc94de1a8879103b1eb971e6f5c1
YasuGoto <yasuko.goto@nijibox.co.jp>
2025-03-10 01:13:58 +0900

    fix: config修正

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  build: {
+    outDir: "front/dist", // 'front' フォルダ内に出力される
+    rollupOptions: {
+      input: "/front/index.html", // Vercelに適切なエントリーポイントを指定
+    },
+  },
+};
